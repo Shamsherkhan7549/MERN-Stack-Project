342c39920ef7ec58fddbcd750301ee3df07e87f9
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-02-12 16:16:50 +0530

    stripe payment feature codded in backen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import ORDERMODEL from "../models/orderSchema.js";
 import USERMODEL from "../models/userModel.js";
+import Stripe from 'stripe'
+
+//Global Variables
+const currency = 'inr';
+const deliveryCharges = 10;
+
+//Gateway initialized
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Placing orders using COD Method
 const placeOrder = async(req, res) => {
@@ -33,7 +41,59 @@ const placeOrder = async(req, res) => {
 
 //Placing orders using Stripe Method
 const placeOrderStripe = async(req, res) => {
+    try{
 
+        const {userId, items, amount, address} = req.body;
+        const{origin} = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod:'stripe',
+            payment:false,
+            date:Date.now()
+        };
+
+        const newOrder = new ORDERMODEL(orderData);
+        await newOrder.save();
+
+        const line_items = items.map((item) => ({
+            price_data:{
+                currency: currency,
+                product_data: {
+                    name:item.name
+                },
+                unit_amount: item.price * 100,
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({ 
+            price_data:{
+            currency: currency,
+            product_data: {
+                name:'Delivery Charges'
+            },
+            unit_amount: deliveryCharges * 100,
+        },
+            quantity: 1,     
+        });
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?false=true&orderId=${newOrder._id}`, 
+            line,items,
+            mode:payment
+        });
+
+        res.json({success:true, session_url:session.url})
+
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
 };
 
 //Placing orders using Razorpay Method
